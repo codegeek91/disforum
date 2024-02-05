2b41525b04abafdcd2ad9b76f1d659fd38bf8298
codegeek1991 <codegeek1991@gmail.com>
2024-02-04 23:09:03 -0500

    adding create topic modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import TopicCreateForm from "@/components/topics/topic-create-form";
+
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div className="grid grid-cols-4 gap-4 p4">
+      <div className="col-span-3">
+        <h1 className="text-xl m2">Top Posts</h1>
+      </div>
+      <div>
+        <TopicCreateForm />
+      </div>
+    </div>
+  );
 }
