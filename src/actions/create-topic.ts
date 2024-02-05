c008a4205c5b79703376fd3893e44b0357903862
codegeek1991 <codegeek1991@gmail.com>
2024-02-04 23:30:58 -0500

    adding zod and testing server side validation

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,10 +1,26 @@
 "use server";
 
-export async function createTopic(formData: FormData) {
-  const name = formData.get("name");
-  const description = formData.get("description");
+import { z } from "zod";
 
-  console.log(name, description);
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/^[a-z-]+$/, {
+      message: "Must be lower case letters or dashes with no spaces",
+    }),
+  description: z.string().min(10),
+});
+
+export async function createTopic(formData: FormData) {
+  const result = createTopicSchema.safeParse({
+    name: formData.get("name"),
+    description: formData.get("description"),
+  });
+
+  if (!result.success) {
+    console.log(result.error.flatten().fieldErrors);
+  }
 
   //TODO: revalidate the homepage
 }
